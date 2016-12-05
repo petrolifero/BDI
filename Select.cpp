de4fe438fd58fc7f78f3fe841ea92ac735ddcad6
Marcelo Muniz <marcelo.pjmuniz@gmail.com>
2016-12-05 16:25:46 -0200

    Tratamento de SQL concluído, geração de .alg concluída, faltando execução do .alg

--- Select.cpp
+++ Select.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <cassert>
 #include <stdlib.h>
+#include <fstream>
 #include <string>
 #include <string.h>
 #include <vector>
@@ -70,6 +71,57 @@ void select(FILE *ponteiroArquivo){
 	free(linha);
 }
 
+
+string escreverProjecao(string relacao, int qtdAtributos, string listaAtributos, string nomeProjecao){
+	fstream arquivo;
+	arquivo.open("Operacao.alg", fstream::app);
+
+	arquivo << "P(" << relacao << "," << qtdAtributos << "," << listaAtributos << "," << nomeProjecao << ")\n";
+
+	arquivo.close();
+
+	return nomeProjecao;
+}
+
+string escreverSelecao(string relacao, string atr, string op, string valor, string nomeSelecao){
+	fstream arquivo;
+	arquivo.open("Operacao.alg", fstream::app);
+	arquivo << "S(" << relacao << "," << atr << "," << op << "," << valor << ", " << nomeSelecao << ")\n";
+	arquivo.close();
+
+	return nomeSelecao;
+}
+
+string escreverJuncao(string relacao, string relacaoB, string condicao, string nomeJuncao){
+	fstream arquivo;
+	arquivo.open("Operacao.alg", fstream::app);
+
+	arquivo << "J(" << relacao << "," << relacaoB << "," << condicao << "," << nomeJuncao << ")\n";
+
+	return nomeJuncao;
+}
+
+string parseAtr(string where){
+	size_t found = where.find_first_of("=<>");	
+
+	return where.substr(0, found);
+}
+
+string parseOp(string where){
+	size_t last = where.find_last_of("=<>"),
+		   first = where.find_first_of("=<>");
+
+	string t(1, where.at(last));
+
+	return ( first == last ) ?  t : where.substr(first, last - first);
+}
+
+string parseVal(string where){
+	size_t found = where.find_last_of("=<>");	
+
+	return where.substr(found + 1, where.length());
+}
+
 /**
  *	Função de parsing do comando passado.
  *
@@ -89,8 +141,11 @@ void identificarComando(string &comando){
 	//Estado inicial: lendo SELECT
 	int estado = 0;
 
-	vector<string> atributos;
-	string relacao, where, relacaoB, condicao;
+	string atributos;
+	string relacao, where, relacaoB, condicao, 
+		   selecao, projecao, juncao;
+
+	
 
 	for (int i = 0; i < clausulas.size(); i++){
 		switch(estado){
@@ -101,7 +156,9 @@ void identificarComando(string &comando){
 				break;
 			case 1:
 				//ler lista de atributos
-				atributos = split(clausulas[i], ',');
+				//atributos = split(clausulas[i], ',');
+				atributos = clausulas[i];
+
 				estado++;
 				break;
 			case 2:
@@ -123,10 +180,15 @@ void identificarComando(string &comando){
 				if(clausulas.size() > 4){
 					estado++;
 				} else{
-					//fim
+					//fim: projeção na relacao, com os atributos
 					boost::erase_all(relacao, ";");
+
+					escreverProjecao(relacao, count(atributos.begin(), atributos.end(), ',') + 1 , 
+							atributos, relacao + "_Proj(" + atributos + ")");
+
 					estado = 99;
 				}
+
 				break;
 			case 4:
 				//condição where
@@ -134,6 +196,13 @@ void identificarComando(string &comando){
 
 				//seleção -join +where
 				where = clausulas[i];
+				boost::erase_all(where, ";");
+				
+				selecao = escreverSelecao(relacao, parseAtr(where), parseOp(where), 
+						parseVal(where), relacao + "_Sel(" + where + ")");
+				escreverProjecao(selecao, count(atributos.begin(), atributos.end(), ',') + 1,
+						atributos, selecao + "_Proj(" + atributos + ")");
+
 				estado = 99;
 				break;
 			case 5:
@@ -142,7 +211,6 @@ void identificarComando(string &comando){
 					estado++;
 					break;
 				}
-
 				relacao = clausulas[i];
 				boost::erase_all(relacao, "(");
 				
@@ -164,7 +232,12 @@ void identificarComando(string &comando){
 					estado++;
 					continue;
 				}
-				
+
+				juncao = escreverJuncao(relacao, relacaoB, condicao, 
+						relacao + "_Jun(" + condicao + ")_" + relacaoB);
+				escreverProjecao(juncao, count(atributos.begin(), atributos.end(), ',') + 1,
+						atributos, juncao + "_Proj(" + atributos + ")");
+
 				estado = 99;
 				break;
 			case 8:
@@ -172,6 +245,14 @@ void identificarComando(string &comando){
 
 				where = clausulas[i];
 				boost::erase_all(where, ";");
+
+				juncao = escreverJuncao(relacao, relacaoB, condicao,
+						relacao + "_Jun(" + condicao + ")_" + relacaoB);
+				selecao = escreverSelecao(juncao, parseAtr(where), parseOp(where), 
+						parseVal(where), juncao + "_Sel(" + where + ")");
+				escreverProjecao(selecao, count(atributos.begin(), atributos.end(), ',') + 1, 
+						atributos, selecao + "_Proj(" + atributos + ")");
+
 				estado = 99;
 				break;
 			default:
@@ -181,7 +262,7 @@ void identificarComando(string &comando){
 
 }
 
-/*int main(int argc, char **argv){
+int main(int argc, char **argv){
 	FILE *arq = fopen(argv[1], "r");
 	select(arq);
 	select(arq);
@@ -189,4 +270,4 @@ void identificarComando(string &comando){
 	select(arq);
 	select(arq);
 	fclose(arq);
-}*/
+}

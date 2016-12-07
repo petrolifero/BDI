1f4e7d7e8978aa72cbf74b6b559ffc6a3e0ea3cb
joao pedro abreu de souza <joaopedroabreudesouza@cryptolab.net>
2016-12-07 16:17:04 -0200

    select resolvido erros

--- src/insert.c
+++ src/insert.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include "type.h"
+#include <stdlib.h>
 
-
-typedef struct
+typedef struct Ctl
 {
 	Type t;
 	int notNull;
 }Ctl;
 
-typedef struct
+typedef struct Input
 {
 	char value[301];
 	Type t;
@@ -25,23 +25,87 @@ int numberOfFields(FILE* fp)
 	return x;
 }
 
-typedef struct
+typedef struct ListInput
 {
 	Input i;
 	struct ListInput* prox;
 }ListInput;
 
-typedef struct
+typedef struct ListCtl
 {
 	Ctl ctl;
 	struct ListCtl* prox;
 }ListCtl;
 
 
+int lenc(ListCtl* l )
+{
+		int count=0;
+		while(l)
+		{
+				count++;
+				l=l->prox;
+		}
+		return count;
+}
+
+int leni(ListInput* l ) 
+{
+        int count=0;
+        while(l)
+        {
+                count++;
+                l=l->prox;
+        }
+        return count;
+}
+
+ListInput* readInput(FILE* sql)
+{
+	fseek(sql,1,SEEK_CUR);	
+}
+
+ListCtl* readCtl(FILE* fp)
+{
+	
+}
+
+int tamFile(FILE* fp)
+{
+	int actual;
+	int len;
+	actual=ftell(fp);
+	fseek(fp,0,SEEK_END);
+	len=ftell(fp);
+	fseek(fp,actual,SEEK_SET);
+	return len;
+}
+
+void writeLine(FILE* fp, ListInput* input)
+{
+}
+
+
+void writeOnFile(ListInput* input, char* tableName)
+{
+		char* fileName=(char*)calloc(strlen(tableName)+1+4,sizeof(char));
+		FILE* fp;
+		strcpy(fileName, tableName);
+		strcat(fileName, ".dad");
+		fp=fopen(fileName, "w+");
+		if(!fp) exit(1);
+		if(tamFile(fp)==0)
+		{
+				writeLine(fp,input);
+		}
+		
+}
+
+
 
 void sql_insert(FILE* sql)
 {
-        char tableName[301], tablectl[304];
+    char tableName[301], tablectl[304];
 	FILE* fp;
 	int aux;
 	int card;
@@ -66,13 +130,12 @@ void sql_insert(FILE* sql)
 	input=readInput(sql);
 	ctl=readCtl(fp);
 
-	if(len(input)!=len(ctl))
+	if(leni(input)!=lenc(ctl))
 		return;
 	for(auxctl=ctl,auxinput=input; auxctl && auxinput; auxctl=auxctl->prox,auxinput=auxinput->prox)
 	{
+		if((auxctl->ctl.notNull && auxinput->i.t==NULLVALUE) || (auxinput->i.t!=NULLVALUE && auxinput->i.t!=auxctl->ctl.t)  )
+				return ;
 	}
-
-
-
-
+	writeOnFile(input,tableName);
 }
